11f95626686cc2d29467066ab71fdaea15159473
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-03 15:36:52 -0300

    Trabalho-Programa-o-Web-2
    Feito menu no arquivo header.mustache utilizando o Bootstrap 5 com auxílio do CSS.

--- webprojeto/app.js
+++ webprojeto/app.js
@@ -1,27 +1,38 @@
+//Importes de pacotes
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Importes de rotas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Cria uma instância express
 var app = express();
 
-// view engine setup
+//Configuração de Templates
+//Importa e instancia o mustache
 const mustacheExpress = require("mustache-express");
 app.engine('mustache', mustacheExpress());
+//define o diretório dos arquivos e a engine
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'mustache');
+//Não armazenar no cache fazer ele caso aja alterações ele rodar as novas alterações
+mustacheExpress.cache = null;
 
+//Configuração do express
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Definição da rota de acesso
 app.use('/', indexRouter);
+//Definição da rota users
 app.use('/users', usersRouter);
 
+//Exporta o express para os outros arquivos do projeto
 module.exports = app;
