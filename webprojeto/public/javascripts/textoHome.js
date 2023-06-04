ee89829190e409ba48dd26ec9637d70936a6966a
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-04 19:38:45 -0300

    Trabalho-Programa-o-Web-2
    Inserção do resumo no home.mustache a partir de um arquivo de texto, mas está com um problema. É necessário remover a barra de rolagem lateral que está sendo exibida.

---
+++ webprojeto/public/javascripts/textoHome.js
@@ -0,0 +1,13 @@
+//Função para carregamento do arquivo txt
+fetch('/descricao/resumo.txt')
+  //carrega o texto do arquivo txt informado pela fetch
+  .then(response => response.text())
+  .then(text => {
+    //Exibindo uma mensagem no navegagor que foi 
+    console.log("Texto exibido na tela");
+    //pego as informações no no mustache onde eu define o pre com id texto
+    document.getElementById('texto').innerText = text.replace(/\n/g, "<br>");
+  })
+  .catch(error => {
+    console.error('Erro ao carregar o arquivo de texto:', error);
+  }); 
