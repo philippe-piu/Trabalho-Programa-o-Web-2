aff8b92ffc4e4eee8be7acb0911c32093acbd30b
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-06 10:20:58 -0300

    Trabalho-Programa-o-Web-2
    feito a formLogim e importada na login.mustache, rotas e o css

--- webprojeto/routes/index.js
+++ webprojeto/routes/index.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+//P. Inicial
 router.get('/', (req, res) => {
   res.render('home');
 });
 
-
+//Sobre
 router.get('/sobre', (req, res) => {
   res.render('sobre');
 });
 
+//Login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+//Home
 router.get('/home', (req, res) => {
   res.render('home');
 });
 
+//Exportar rotas
 module.exports = router;
