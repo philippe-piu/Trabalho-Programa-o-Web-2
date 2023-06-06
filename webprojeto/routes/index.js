9a92a1e801a91d2f8cc3a58428281d8245b4c780
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-06 14:37:22 -0300

    Trabalho-Programa-o-Web-2
    foi feito a intalação do joi  e @hapi/joi para fazer a validação do formulario login foi criado uma função para validar email e senha esses doi itens juntos em um arquivo javascrip e foi feita uma alteração na routes colocacando um validados para me mostar erro se na hora de logar tiver algum proble e se deu certo vai para um pagina que no momento ta sobre e já deixei um email e senha definido para teste e mechido no formLogin para pegar as novas informações de validação.

--- webprojeto/routes/index.js
+++ webprojeto/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var TaskSchema = require("../validador_Formulario/TaskSValidador");
 
 //P. Inicial
 router.get('/', (req, res) => {
@@ -19,6 +20,30 @@ router.get('/login', (req, res) => {
 //Home
 router.get('/home', (req, res) => {
   res.render('home');
+});
+
+//Validação do Formulario
+router.post('/sobre', (req, res) => {
+  const { error, value } = TaskSchema.validate(req.body);
+  //condicional para verificação de erros
+  if (error) {
+    //mensagem de erro no console.
+    console.log("Erro de validação");
+    //mensagem para usuario
+    res.send("Erro de validação");
+  } else {
+
+    const email = value.email;
+    const senha = value.senha;
+
+    //condicional para verificar se o email e senha já definido neste momento é valido ou sendo ou não valido ele imprimi mensagens definidas abaixo.
+    if (email === "teste@gmail.com" && senha === "teste123") {
+      //res.send("Email e senha válidos");
+      res.render('sobre');
+    } else {
+      res.send("Erro no login. Verifique suas informações");
+    }
+  }
 });
 
 //Exportar rotas
