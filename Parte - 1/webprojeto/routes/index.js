1d84677c17c625224b1ac1b49a2f64f32fbed8e9
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-11 20:38:05 -0300

    Trabalho-Programa-o-Web-2
    Mudado praticamente tudo refeito algumas partes do front-end

---
+++ Parte - 1/webprojeto/routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var TaskSchema = require("../public/javascripts/TaskSValidador");
+
+/*Rota Inicial */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+//Rota Register
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+//Rota Home
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+//Rota About
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+//Rota COmunity
+router.get('/comunity', function(req, res, next) {
+  res.render('comunity');
+});
+
+//Rota Store
+router.get('/store', function(req, res, next) {
+  res.render('store');
+});
+
+//Validação do Formulario
+router.post('/home', (req, res) => {
+  const { error, value } = TaskSchema.validate(req.body);
+  //condicional para verificação de erros
+  if (error) {
+    //mensagem de erro no console.
+    console.log("Erro de validação");
+    //mensagem para usuario
+    res.send("Erro de validação");
+  } else {
+
+    const email = value.email;
+    const senha = value.senha;
+
+    //condicional para verificar se o email e senha já definido neste momento é valido ou sendo ou não valido ele imprimi mensagens definidas abaixo.
+    if (email === "teste@gmail.com" && senha === "teste123") {
+      //res.send("Email e senha válidos");
+      res.render('home');
+    } else {
+      res.send("Erro no login. Verifique suas informações");
+    }
+  }
+});
+
+module.exports = router;
