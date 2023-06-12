9dec2fed944c448ac2487d1771d0cdb8831b3270
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-12 11:00:30 -0300

    Trabalho-Programa-o-Web-2
    Feito as paginas de about, tecnologies e description e seus respectivos templates.

--- Parte - 1/webprojeto/routes/index.js
+++ Parte - 1/webprojeto/routes/index.js
@@ -1,59 +1,68 @@
-var express = require('express');
-var router = express.Router();
-var TaskSchema = require("../public/javascripts/TaskSValidador");
+var express = require('express')
+var router = express.Router()
+var TaskSchema = require('../public/javascripts/TaskSValidador')
 
 /*Rota Inicial */
-router.get('/', function(req, res, next) {
-  res.render('login');
-});
+router.get('/', function (req, res, next) {
+  res.render('login')
+})
 
 //Rota Register
-router.get('/register', function(req, res, next) {
-  res.render('register');
-});
+router.get('/register', function (req, res, next) {
+  res.render('register')
+})
 
 //Rota Home
-router.get('/home', function(req, res, next) {
-  res.render('home');
-});
+router.get('/home', function (req, res, next) {
+  res.render('home')
+})
 
 //Rota About
-router.get('/about', function(req, res, next) {
-  res.render('about');
-});
+router.get('/about', function (req, res, next) {
+  res.render('about')
+})
+
+//Rota Technologies
+router.get('/technologies', function (req, res, next) {
+  res.render('technologies')
+})
+
+//Rota Description Author
+router.get('/description', function (req, res, next) {
+  res.render('description')
+})
 
 //Rota COmunity
-router.get('/comunity', function(req, res, next) {
-  res.render('comunity');
-});
+router.get('/comunity', function (req, res, next) {
+  res.render('comunity')
+})
 
 //Rota Store
-router.get('/store', function(req, res, next) {
-  res.render('store');
-});
+router.get('/store', function (req, res, next) {
+  res.render('store')
+})
 
 //Validação do Formulario
 router.post('/home', (req, res) => {
-  const { error, value } = TaskSchema.validate(req.body);
+  const { error, value } = TaskSchema.validate(req.body)
   //condicional para verificação de erros
   if (error) {
     //mensagem de erro no console.
-    console.log("Erro de validação");
+    console.log('Erro de validação')
     //mensagem para usuario
-    res.send("Erro de validação");
+    res.send('Erro de validação')
   } else {
-
-    const email = value.email;
-    const senha = value.senha;
+    const email = value.email
+    const senha = value.senha
 
     //condicional para verificar se o email e senha já definido neste momento é valido ou sendo ou não valido ele imprimi mensagens definidas abaixo.
-    if (email === "teste@gmail.com" && senha === "teste123") {
+    if (email === 'teste@gmail.com' && senha === 'teste123') {
       //res.send("Email e senha válidos");
-      res.render('home');
+      res.render('home')
     } else {
-      res.send("Erro no login. Verifique suas informações");
+      res.send('Erro no login. Verifique suas informações')
     }
   }
-});
+})
 
-module.exports = router;
+module.exports = router
