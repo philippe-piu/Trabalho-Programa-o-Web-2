55275a724b23df9bbd214ca68f07c8cbfd9a34c1
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-06-22 20:27:15 -0300

    Trabalho-Programa-o-Web-2
    lembrete para pesquisar:  está dando este erro Cannot destructure property 'nome' of 'req.body' as it is undefined-

--- Parte - 2/projetoWeb/src/back-end/routers/index.js
+++ Parte - 2/projetoWeb/src/back-end/routers/index.js
@@ -1,12 +1,37 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const {chamadaForm} = require('../../front-end/javascripts/contactForm')
 
+//Rotas Dinâmicas
 //Rota Principal
 router.get('/', (req, res) => {
-  
-  res.render(path.join(__dirname, '../../front-end/views/dynamic/login'));
-});
+  res.render(path.join(__dirname, '../../front-end/views/dynamic/login'))
+})
 
-module.exports = router;
+//Rota Contatic
+router.get('/contact', function (req, res, next) {
+  res.render(path.join(__dirname, '../../front-end/views/dynamic/contact'))
+})
 
+
+//Rotas Estáticas
+//Rota Description Author
+router.get('/description', function (req, res, next) {
+  res.render(path.join(__dirname, '../../front-end/views/static/description'))
+})
+
+//Rota About
+router.get('/about', function (req, res, next) {
+  res.render(path.join(__dirname, '../../front-end/views/static/about'))
+})
+
+//Rota About
+router.get('/technologies', function (req, res, next) {
+  res.render(path.join(__dirname, '../../front-end/views/static/technologies'))
+})
+
+//Endereço envio de mensagem
+router.post('/contact/teste', chamadaForm);
+
+module.exports = router
